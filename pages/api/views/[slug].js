77e78994e52d01f4ef9fe1c275b2c75ef7790957
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2022-02-09 16:59:50 -0500

    added mongodb, lib and util functions

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -1,10 +1,12 @@
 const handler = async (req, res) => {
+    const { slug } = req.query
+
     if (req.method === 'GET') {
-        return res.status(200).json({ message: 'success' })
+        return res.status(200).json({ slug })
     }
 
     if (req.method === 'POST') {
-        return res.status(200).json({ message: 'success' })
+        return res.status(200).json({ slug })
     }
 }
 
---
+++ utils/mongodb.js
@@ -0,0 +1,44 @@
+import clientPromise from '../lib/mongodb'
+
+let client
+let db
+
+async function init() {
+    if (db) return
+    try {
+        client = await clientPromise
+        db = await client.db()
+    } catch (error) {
+        throw new Error('Failed to stablish connection to database')
+    }
+}
+
+/////////////
+/// VIEWS ///
+/////////////
+
+export async function getViews(slug) {
+    try {
+        await init()
+        const views = await db.collection('views')
+        const view = await views.findOne({ slug })
+        return { count: view ? view.count : 0 }
+    } catch (error) {
+        return { error }
+    }
+}
+
+export async function addView(slug) {
+    try {
+        await init()
+        const views = await db.collection('views')
+        const view = views.findOneAndUpdate(
+            { slug },
+            { $inc: { count: 1 } },
+            { returnDocument: 'after', upsert: true }
+        )
+        return { count: view ? view.count : 0 }
+    } catch (error) {
+        return { error }
+    }
+}
