2eecbff7c6d4a6bb55e35db26d9caad5b72c7e9f
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2022-02-09 16:07:52 -0500

    removed firebase

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -1,23 +1,10 @@
-import db from '@/lib/firebase'
-
 const handler = async (req, res) => {
     if (req.method === 'GET') {
-        const snapshot = await db.ref('views').child(req.query.slug).once('value')
-        const views = snapshot.val()
-
-        return res.status(200).json({ total: views })
+        return res.status(200).json({ message: 'success' })
     }
 
     if (req.method === 'POST') {
-        const ref = db.ref('views').child(req.query.slug)
-        const { snapshot } = await ref.transaction(views => {
-            if (views === null) {
-                return 1
-            }
-            return views + 1
-        })
-
-        return res.status(200).json({ total: snapshot.val() })
+        return res.status(200).json({ message: 'success' })
     }
 }
 
