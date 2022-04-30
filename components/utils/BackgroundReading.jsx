178c0a0b9d233a70a32b3cadd10f6d97474a1985
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2022-04-30 11:54:40 -0400

    Added csrf-attacks note, and minor change to BackgroundReading component

--- components/utils/BackgroundReading.jsx
+++ components/utils/BackgroundReading.jsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 
 const BackgroundReading = ({ href, title, children }) => {
+  const isInternalLink =
+    href && (href.startsWith('/') || href.startsWith('#'))
+
   return (
-    <div className="flex max-w-max flex-col gap-1 rounded-md bg-gray-200 bg-opacity-75 px-5 py-4 text-sm dark:bg-react-link dark:bg-opacity-20 sm:flex-row sm:gap-4">
+    <div className="flex max-w-max flex-col gap-1 rounded-md bg-gray-200 bg-opacity-75 px-5 py-4 text-sm dark:bg-gray-600 dark:bg-opacity-20 sm:flex-row sm:gap-4">
       <span className="text-gray-500">{title}</span>
-      <Link href={href}>
-        <a>{children}</a>
-      </Link>
+      {isInternalLink ? (
+        <Link href={href}>
+          <a>{children}</a>
+        </Link>
+      ) : (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      )}
     </div>
   )
 }
